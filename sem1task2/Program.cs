cbf75f986c00872f540c93cf4d2432c2da3a76d4
ZykinaViktoriya <zykina.viktoriya@gmail.com>
2023-02-28 09:19:17 +0300

    new tasks

---
+++ sem1task2/Program.cs
@@ -0,0 +1,70 @@
+﻿System.Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        System.Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        System.Console.WriteLine("Вторник");
+        break;
+    case 3:
+        System.Console.WriteLine("Среда");
+        break;
+    case 4:
+        System.Console.WriteLine("Четверг");
+        break;
+    case 5:
+        System.Console.WriteLine("Пятница");
+        break;
+    case 6:
+        System.Console.WriteLine("Суббота");
+        break;
+    case 7:
+        System.Console.WriteLine("Воскресенье");
+        break;
+    default:
+        System.Console.WriteLine("Такого дня недели не существует");
+        break;
+
+}
+// if (number == 1)
+// {
+//     System.Console.WriteLine("Понедельник");
+// }
+
+// else if (number == 2)
+// {
+//     System.Console.WriteLine("Вторник");
+// }
+
+// else if (number == 3)
+// {
+//     System.Console.WriteLine("Среда");
+// }
+
+// else if (number == 4)
+// {
+//     System.Console.WriteLine("Четверг");
+// }
+
+// else if (number == 5)
+// {
+//     System.Console.WriteLine("Пятница");
+// }
+
+// else if (number == 6)
+// {
+//     System.Console.WriteLine("Суббота");
+// }
+
+// else if (number == 7)
+// {
+//     System.Console.WriteLine("Воскресенье");
+// }
+
+// else
+// {
+//     System.Console.WriteLine("Такого дня недели не существует");
+// }
---
+++ sem1task3/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите число N ");
+ int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+for (int i = -number; i <= number; i++)
+{
+    System.Console.Write(i + " ");
+}
